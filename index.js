ae449ee4e2fb1e6d666001624628bb4b33368c03
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-06 16:53:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,63 +3,22 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setPresence({game:{name:'munching and crunching',type:0}});﻿
-});
-
-// Event to listen to messages sent to the server where the bot is located
-bot.on('message', message => {
-    // So the bot doesn't reply to iteself
-    if (message.author.bot) return;
-
-    if (message.content.indexOf("I love you Yuki") === 0) {
-    return message.channel.send("I love you too, Satsujin");
-    }
-
-    if (message.content.indexOf("Yuki is hot") === 0) {
-    return message.channel.send("I know.");
-    }
-
-    if (message.content.indexOf("Yuppi") === 0) {
-    return message.channel.send("Gakkyun");
-    }
-
-    if (message.content.indexOf("I can't believe Ryo is dead") === 0) {
-    return message.channel.send("Finally.");
-    }
+  bot.user.setActivity(`Hey, Wassup!`);
 });
 
 bot.on("message", async message => {
-  if(message.author.box) return;
-  if(message.channel.type === "dm") return;
 
-  let prefix = botconfig.prefix;
-  let messageArray = message.content.split( " ");
-  let cmd = messageArray[0]
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if(cmd ===`${prefix}botinfo`){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#00ff67")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-
-    return message. channel.send(botembed);
+  if (cmd === `${prefix}ping`){
+    message.channel.send("Pong!");
   }
-
-  if(cmd === `${prefix}mafia`){
-    return message.channel.send("maf maf");
-  }
-
-  if(cmd === `${prefix}leaf`){
-    return message.channel.send("babe, that me leaf.");
-  }
-
-  if(cmd === `${prefix}ryo`){
-    return message.channel.send("Oh fuck. Bastard spotted.");
-  }
-
 });
+
 bot.login(process.env.token);
