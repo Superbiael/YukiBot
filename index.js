7b9cf1fbb5d95d8e4e0562a423e06e84a2f8210f
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-06 17:05:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setPresence({game:{name:'munching and crunching',type:0}});﻿
+  bot.user.setPresence({game:{name:'testing',type:0}});﻿
 });
 
 // Event to listen to messages sent to the server where the bot is located
