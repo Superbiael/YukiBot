6441979790fe702bd9e4dd225ce4c639bdaf5c71
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-07 00:16:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,25 @@ bot.on("message", async message => {
   if(cmd === `${prefix}car`){
     return message.channel.send("Papa, buy me a car.");
   }
+  
+  if(cmd === `${prefix}crank`){
+    return message.channel.send("We’ll have a secret intensive training session together.");
+  }
+  
+  if(cmd === `${prefix}brank`){
+    return message.channel.send("What’s wrong, that you can’t do something like this?.");
+  }
+  
+  if(cmd === `${prefix}arank`){
+    return message.channel.send("Next time, I expect you will try harder.");
+    
+  if(cmd === `${prefix}srank`){
+    return message.channel.send("I’m going to imitate Momo. Yay! You did it!");
+  }
 
+  if(cmd === `${prefix}ssrank`){
+    return message.channel.send("As expected. I’m also pleased.");
+  }
 });
 
 bot.login(process.env.token);
