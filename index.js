0fa941328958640f8e2aea24cf8e1619c138ec44
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-06 17:10:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ bot.on("ready", async () => {
   bot.user.setActivity(`Testing1`);
 });
 
+// Event to listen to messages sent to the server where the bot is located
+bot.on('message', message => {
+    // So the bot doesn't reply to iteself
+    if (message.author.bot) return;
+
+    if (message.content.indexOf("I love you Yuki") === 0) {
+    return message.channel.send("I love you too, Satsujin");
+    }
+
+    if (message.content.indexOf("Yuki is hot") === 0) {
+    return message.channel.send("I know.");
+    }
+
+    if (message.content.indexOf("Yuppi") === 0) {
+    return message.channel.send("Gakkyun");
+    }
+
+    if (message.content.indexOf("I can't believe Ryo is dead") === 0) {
+    return message.channel.send("Finally.");
+    }
+});
+
 bot.on("message", async message => {
 
   if (message.author.bot) return;
