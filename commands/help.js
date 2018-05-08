485dcf78683acf7d7e7c4b126df5306754222591
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-07 22:24:42 -0700

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot,message, args) => {
+
+  let helpembed = new Discord.RichEmbed()
+  .setColor("#00ff67")
+  .addField("y!Commands", "mafia (alias:maf), leaf, ryo, order,car, c - ss rank")
+  .addField("Other Commands", "I can't believe Ryo is dead, I'm gonna munch, Yuppi, I love you Yuki, Yuki is hot, Good morning Yuki, Good night Yuki");
+
+  message.channel.send(helpembed);
+}
+
+module.exports.help = {
+  name: "help"
+}
