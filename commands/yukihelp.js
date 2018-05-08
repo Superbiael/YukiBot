5d9ba14679cef0884d2b534982dd5233bf02c46c
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 01:09:58 -0700

    Update yukihelp.js

--- commands/yukihelp.js
+++ commands/yukihelp.js
@@ -4,9 +4,9 @@ module.exports.run = async (bot,message, args) => {
 
   let helpembed = new Discord.RichEmbed()
   .setColor("#00ff67")
-  .addField("y!Commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
-  .addField("Other Commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there, Pika~")
-  .addField("8ball Command:","y!yuki <question>");
+  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
+  .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there, Pika~")
+  .addField("8ball command:","y!yuki <question>");
 
   message.channel.send(helpembed);
 }
