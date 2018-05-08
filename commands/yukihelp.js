eb9022df442d79b59d62c9257fab02824bf78804
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 00:32:59 -0700

    Update yukihelp.js

--- commands/yukihelp.js
+++ commands/yukihelp.js
@@ -6,12 +6,12 @@ module.exports.run = async (bot,message, args) => {
   .setDescription(" * - case sensitive ")
   .setColor("#00ff67")
   .addField("y!Commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
-  .addField("Other Commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Yuki is hot || Good morning Yuki || Good night Yuki || Yuki, if you're out there, *Pika~")
+  .addField("Other Commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there, *Pika~")
   .addField("8ball Command:","y!yuki <question>");
 
   message.channel.send(helpembed);
 }
 
 module.exports.help = {
-  name: "yukihelp"
+  name: "yhelp"
 }
