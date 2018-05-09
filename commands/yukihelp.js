a437f61dde13af4abecd05a52e99f3587e145d74
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-08 19:01:16 -0700

    Update yukihelp.js

--- commands/yukihelp.js
+++ commands/yukihelp.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message, args) => {
 
   let helpembed = new Discord.RichEmbed()
+  .setDescription("Do not include < > when using commands.")
   .setColor("#00ff67")
   .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank")
   .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Pika~")
