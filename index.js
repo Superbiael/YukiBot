7df8a69606d6c4f1e71b451751d466ab747cb08d
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 16:35:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -112,9 +112,10 @@ bot.on("message", async message => {
   return message. channel.send(botembed);
   }
 
-  if(cmd === `${prefix}watch out`){
+  if(cmd === `${prefix}watchout`){
   return message.channel.send ("You better watch out.", {files:["./images/youbetterwatchout.png"]});
   }  
+  
   if(cmd === `${prefix}mafia` || cmd === `${prefix}maf`){
     return message.channel.send("maf maf");
   }
