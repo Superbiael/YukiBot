7bc78e7afb21c43a4e4b97f3d560058aa2c92a8f
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-09 19:32:21 -0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No");
   let botmessage = args.join(" ");
   message.delete().catch();
   message.channel.send(botmessage);
