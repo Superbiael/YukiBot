8737aa642ece34d5e91c3bd62ea4ab7f32c1bdae
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-11 13:53:48 -0700

    Update yukihelp.js

--- commands/yukihelp.js
+++ commands/yukihelp.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot,message, args) => {
   let helpembed = new Discord.RichEmbed()
   .setDescription("Do not include < > when using commands.")
   .setColor("#00ff67")
-  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank || watchout || say <message> ")
+  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank || watchout || scout || say <message> ")
   .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Pika~ || Hey gays")
   .addField("8ball command:","y!yuki <question>")
   .addField("Send DM:", "y!send @user <message> | Requires administrator permissions to delete command message after sending DM");
