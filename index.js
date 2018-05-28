6e3ac568d3bee211e37b5ecd9aaa16bec97d543e
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-28 03:06:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ bot.on("message", async message => {
     .setDescription("Do not include < > when using commands. \nCommand phrases are not caps sensitive")
     .setColor("#00ff67")
     .addField("Commands:","**y!yuki** *<question>* | Ask him anything. \n**y!send** *<@user> <message>* | Send a DM to the mentioned user\n**y!scout** | Solo Yolo \n**y!quote** | Random quote\n**y!say** *<message>* | Have the bot say anything you want\n**y!help** | Displays this help message")
-    .addField("Basic y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank *(ex: ss!rank)* || watchout")
+    .addField("Basic y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank *(ex: y!ssrank)* || watchout")
     .addField("Command phrases:", "I can't believe Ryo is dead || I'm gonna munch|| I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Hey gays")
 
     message.channel.send(helpembed);
