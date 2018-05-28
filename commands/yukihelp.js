0ceaf6ed003f246636cd1fe8002b2ce616ac400d
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-05-28 00:48:39 -0700

    Update yukihelp.js

--- commands/yukihelp.js
+++ commands/yukihelp.js
@@ -3,12 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot,message, args) => {
 
   let helpembed = new Discord.RichEmbed()
-  .setDescription("Do not include < > when using commands.")
+  .setDescription("Do not include < > when using commands. \nCommand phrases are not caps sensitive")
   .setColor("#00ff67")
-  .addField("y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank || watchout || scout || say <message> ")
-  .addField("Other commands:", "I can't believe Ryo is dead || I'm gonna munch || Yuppi || I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Pika~ || Hey gays")
-  .addField("8ball command:","y!yuki <question>")
-  .addField("Send DM:", "y!send @user <message> | Requires administrator permissions to delete command message after sending DM");
+  .addField("Commands:","**y!yuki** *<question>* | Ask him anything. \n**y!send** *<@user> <message>* | Send a DM to the mentioned user\n**y!scout** | Solo Yolo \n**y!quote** | Random quote\n**y!say** *<message>* | Have the bot say anything you want\n**y!yukihelp** | Displays this help message")
+  .addField("Basic y!commands:", "mafia (alias:maf) || leaf || ryo || order || car || c - ss rank || watchout")
+  .addField("Command phrases:", "I can't believe Ryo is dead || I'm gonna munch|| I love you Yuki || Good morning Yuki || Good night Yuki || Yuki if you're out there || Hey gays")
 
   message.channel.send(helpembed);
 }
