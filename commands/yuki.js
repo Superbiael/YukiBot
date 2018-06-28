e2195d5b1bd3edf2d5e130170645cec424986754
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-06-27 19:10:02 -0700

    Update yuki.js

--- commands/yuki.js
+++ commands/yuki.js
@@ -2,17 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
    if(!args[0]) return message.reply("I'm gay, and can't read.");
-   let replies = ["Well, yeah. I guess", "That's right.", "No... is what you thought I would say (lol)","No way.", "Yes... Is what you thought I would say (lol)","Of course.", "Don't be ridiculous.","Do I look like someone who'd know?", "Sure, why not?", "A whole lot.", "More or less."];
+   let replies = ["That's right.", "No... is what you thought I would say (lol)","No way.", "Yes... Is what you thought I would say (lol)","Of course.", "Don't be ridiculous.","Do I look like someone who'd know?", "Sure, why not?", "A whole lot.", "More or less."];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
 
    let ballembed = new Discord.RichEmbed()
+   .setAuthor(message.author.tag)
    .setColor("#00ff67")
    .addField("Question:", question)
    .addField("Answer:", replies[result]);
 
-   message.channel.send(ballembed);
+   let botschannel = message.guild.channels.find(`name`, "idolboti7");
+   if(!botschannel) return message.channel.send("Couldn't find bot channel.");
+   botschannel.send(ballembed);
 }
 
 module.exports.help = {
