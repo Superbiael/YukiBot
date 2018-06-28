6823a1aa072bfc893dae60f78fc277c535cd8c71
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-06-27 17:51:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ bot.on('message', message => {
      var random = Math.floor (Math.random() * (number)) + 1;
      switch (random) {
        case 1: message.channel.send ("You remind me of a puppy, just like Momo. No, maybe you're more like a tiny baby rabbit?"); break;
-       case 2: message.channel.send ("I'm a vegetarian. Not for spirtual reasons, just because I prefer that sort of food."); break;
+       case 2: message.channel.send ("I'm a vegetarian. Not for spiritual reasons, just because I prefer that sort of food."); break;
        case 3: message.channel.send ("If I have the time to eat breakfast, I'd rather spend it sleeping..."); break;
        case 4: message.channel.send ("I've used the \"relative's funeral\" excuse so many times over my school years that I don't remember how much of my family is supposed to be dead by now. I need to come up with something else. \nMaybe I'll go with some kind of illness."); break;
        case 5: message.channel.send ("What were we talking about again? How great I am at cooking?"); break;
@@ -89,13 +89,11 @@ bot.on('message', message => {
        case 20: message.channel.send ("All of Re:vale, including Okarin, are sadists. Chemistry-wise, we make a terrible combination."); break;
        case 21: message.channel.send ("So all I need to do is follow him home without being noticed..."); break;
        case 22: message.channel.send ("What I like about myself.\nThe first thing that comes to mind is that I've been singing for over 10 years now.\nI never stopped, even when I was going through hard times. It's all thanks to Momo, Ban, and Okarin, though."); break;
-       case 23: message.channel.send ("Emotions are something that can't be seen, yet easily changeable, unstable things. I'm sure youre environment growing up is what make you this way. \nYou have nothing to be afraid of. You can come right at us."); break;         
+       case 23: message.channel.send ("Emotions are something that can't be seen, yet easily changeable, unstable things. I'm sure your environment growing up is what make you this way. \nYou have nothing to be afraid of. You can come right at us."); break;         
     }
   }
     
     if(msg.startsWith (prefix + "scout")) {
-      // let emoji = message.guild.emojis.find('name', "yuppi");
-      // message.react(emoji);
         number = 39;
         var random = Math.floor (Math.random() * (number)) + 1;
         switch (random) {
@@ -180,8 +178,6 @@ bot.on('message', message => {
 
   if (msg.startsWith ("i love you yuki")) {
     if(message.author.id != "182723698711592960") return;
-    let emoji = message.guild.emojis.find('name', "yuppismooch");
-    message.react(emoji);
     number = 2;
     var random = Math.floor (Math.random() * (number)) + 1;
     switch (random) {
