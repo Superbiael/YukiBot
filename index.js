e8cf1fcff8df652d00d4118ea0534197c1c1a279
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-06-30 23:27:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("I love you too Satsujin", {type:0});
+  bot.user.setActivity("I love you Satsujin", {type:0});
 });
 
 bot.on('message', message => {
