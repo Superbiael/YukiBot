b9128bacf72c7e0e3a912168a1c93ce0bb8aa7fc
Superbiael <39025166+Superbiael@users.noreply.github.com>
2018-10-16 23:32:35 -0700

    Update yuki.js

--- commands/yuki.js
+++ commands/yuki.js
@@ -1,22 +1,31 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-   if(!args[0]) return message.reply("I'm gay, and can't read.");
-   let replies = ["That's right.", "No... is what you thought I would say (lol)","No way.", "Yes... Is what you thought I would say (lol)","Of course.", "Don't be ridiculous.","Do I look like someone who'd know?", "Sure, why not?", "A whole lot.", "More or less."];
+   if(!args[0]) return message.reply("I'll answer whatever. Will you finally ask for my body measurements?");
+   let replies = [
+     "That's right.",
+     "No... is what you thought I would say (lol)","No way.",
+     "Yes... Is what you thought I would say (lol)","Of course.",
+     "Don't be ridiculous.",
+     "Do I look like someone who'd know?",
+     "Sure, why not?",
+     "A whole lot.",
+     "More or less."
+   ];
 
    let result = Math.floor((Math.random() * replies.length));
    let question = args.slice(0).join(" ");
 
    let ballembed = new Discord.RichEmbed()
-   .setColor("#00ff67")
-   .addField("Question:", question)
-   .addField("Answer:", replies[result]);
+    .setColor("#00ff67")
+    .addField("Question:", question)
+    .addField("Answer:", replies[result]);
 
-    let botschannel = message.guild.channels.find(`name`, "bot-channel");
-    if(!botschannel) return;
-   return botschannel.send(ballembed);
-}
+   let botschannel = message.guild.channels.find(`name`, "bot-channel");
+   if(!botschannel) return;
+   return message.channel.send(ballembed);
+ }
 
 module.exports.help = {
   name:"yuki"
-}
+  }
